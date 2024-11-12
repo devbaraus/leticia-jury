789404016641a30c4d1d76e934e617af77cfd036
Bruno de Araujo Alves <me@baraus.dev>
2024-11-12 06:15:06 -0300

    feat: header

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	const links = [
+		{
+			label: 'Home',
+			href: '/'
+		}
+	];
+</script>
+
+<header class="navbar max-auto container">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<button aria-label="logo" class="btn btn-ghost lg:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor">
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16" />
+				</svg>
+			</button>
+			<ul
+				class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+				{#each links as link (link.href)}
+					<li>
+						<a href={link.href}>{link.label}</a>
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<a href="/" class="btn btn-ghost text-xl">Leticia Jury</a>
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<nav>
+			<menu class="menu menu-horizontal px-1">
+				{#each links as link (link.href)}
+					<li>
+						<a class="text-lg" href={link.href}>{link.label}</a>
+					</li>
+				{/each}
+			</menu>
+		</nav>
+	</div>
+	<div class="navbar-end">
+		<a href="/contato" class="btn btn-primary">Contato</a>
+	</div>
+</header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,42 @@
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
 import type { Config } from 'tailwindcss';
+import themes from 'daisyui/src/theming/themes';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		container: {
+			center: true,
+			padding: {
+				sm: '0.5rem',
+				md: '0'
+			}
+		},
+		extend: {}
+	},
+	daisyui: {
+		themes: [
+			{
+				main: {
+					...themes.light,
+					primary: '#FFD305',
+					'--rounded-box': '1rem', // border radius rounded-box utility class, used in card and other large boxes
+					'--rounded-btn': '0.2rem', // border radius rounded-btn utility class, used in buttons and similar element
+					'--rounded-badge': '1.9rem', // border radius rounded-badge utility class, used in badges and similar
+					'--animation-btn': '0.25s', // duration of animation when you click on button
+					'--animation-input': '0.2s', // duration of animation for inputs like checkbox, toggle, radio, etc
+					'--btn-focus-scale': '0.95', // scale transform of button when you focus on it
+					'--border-btn': '1px', // border width of buttons
+					'--tab-border': '1px', // border width of tabs
+					'--tab-radius': '0.5rem' // border radius of tabs
+				}
+			}
+		]
+	},
 
-  plugins: [typography, forms]
+	plugins: [typography, forms, daisyui]
 } satisfies Config;
